968604889da68e0481c9ea2aa0a139a51dea71ab
diodonfrost <diodon.frost@diodonfrost.me>
2023-07-16 19:27:10 +0200

    chore(examples): run terraform fmt linter

--- examples/main/main.tf
+++ examples/main/main.tf
@@ -13,13 +13,13 @@ resource "azurerm_resource_group" "this" {
 module "azure-function-http-redirect" {
   source = "../../"
 
-  function_app_name_prefix      = "fpn-${random_pet.suffix.id}"
-  service_plan_name             = "spn-${random_pet.suffix.id}"
-  storage_account_name          = "san${random_id.suffix.hex}"
-  resource_group_name           = azurerm_resource_group.this.name
-  location                      = azurerm_resource_group.this.location
-  redirect_mappings             = {
-    "foo.example.com": "https://foo.com",
-    "bar.example.com": "https://bar.com",
+  function_app_name_prefix = "fpn-${random_pet.suffix.id}"
+  service_plan_name        = "spn-${random_pet.suffix.id}"
+  storage_account_name     = "san${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  redirect_mappings = {
+    "foo.example.com" : "https://foo.com",
+    "bar.example.com" : "https://bar.com",
   }
 }
