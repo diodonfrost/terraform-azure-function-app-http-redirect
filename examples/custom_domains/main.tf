2b18b2f4901734e900489f72c12c1362fc1f2232
diodonfrost <diodon.frost@diodonfrost.me>
2023-07-16 19:24:29 +0200

    tests(terraform): provdes example with custom domains
    Show how to create this module with custom domains.

---
+++ examples/custom_domains/main.tf
@@ -0,0 +1,37 @@
+resource "random_pet" "suffix" {}
+
+resource "random_id" "suffix" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "test-${random_pet.suffix.id}"
+  location = "East US"
+}
+
+
+module "azure-function-http-redirect" {
+  source = "../../"
+
+  function_app_name_prefix = "fpn-${random_pet.suffix.id}"
+  service_plan_name        = "spn-${random_pet.suffix.id}"
+  storage_account_name     = "san${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.this.name
+  location                 = azurerm_resource_group.this.location
+  redirect_mappings = {
+    "foo.contoso.com" : "https://foo.com",
+    "bar.contoso.com" : "https://bar.com",
+  }
+  custom_domains = {
+    custom_domain_1 = {
+      name                         = "foo"
+      dns_zone_name                = "contoso.com"
+      dns_zone_resource_group_name = "dns_zone"
+    }
+    custom_domain_2 = {
+      name                         = "bar"
+      dns_zone_name                = "contoso.com"
+      dns_zone_resource_group_name = "dns_zone"
+    }
+  }
+}
